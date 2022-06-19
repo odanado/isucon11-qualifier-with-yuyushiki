cdbb5a6fd32f5d4d6ef0aacc3c7768ded8690ab5
mizdra <pp.mizdra@gmail.com>
2022-06-19 18:27:45 +0900

    デプロイスクリプト修正

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,7 +1,7 @@
 #/!bin/bash
 
 cd ../bench
-/bench -all-addresses 127.0.0.11 -target 127.0.0.11:443 -tls -jia-service-url http://127.0.0.1:4999
+./bench -all-addresses 127.0.0.11 -target 127.0.0.11:443 -tls -jia-service-url http://127.0.0.1:4999
 
 cd ../webapp
 sudo cp /var/log/nginx/access.log logs/access.log
