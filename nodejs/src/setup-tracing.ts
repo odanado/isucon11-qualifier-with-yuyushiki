6fb46717645e1a74f443e5156857e5d494a394fe
odan <odan3240@gmail.com>
2022-06-19 21:02:04 +0900

    samplingRate を0に

--- nodejs/src/setup-tracing.ts
+++ nodejs/src/setup-tracing.ts
@@ -2,5 +2,5 @@ import * as TraceAgent from "@google-cloud/trace-agent";
 
 export function setupTracing(name: string): TraceAgent.PluginTypes.Tracer {
   console.log({ name });
-  return TraceAgent.start({ enhancedDatabaseReporting: true, logLevel: 4 });
+  return TraceAgent.start({ enhancedDatabaseReporting: true, samplingRate: 0 });
 }
