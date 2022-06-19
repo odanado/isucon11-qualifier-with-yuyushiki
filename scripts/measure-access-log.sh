4c1fcd0d280195eae176ecac6b63b70ce49b979d
mizdra <pp.mizdra@gmail.com>
2022-06-19 17:15:45 +0900

    修正

--- scripts/measure-access-log.sh
+++ scripts/measure-access-log.sh
@@ -4,4 +4,4 @@
 LOG_FILE=logs/access-$(date "+%H:%M:%S").log
 
 cp /var/log/nginx/access.log $LOG_FILE
-cat $LOG_FILE alp ltsv -r --sort=sum -m "/api/condition/*,/api/isu/*"
+cat $LOG_FILE | ./alp ltsv -r --sort=sum -m "/api/condition/*,/api/isu/*"
