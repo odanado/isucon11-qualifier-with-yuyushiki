0b1d00d80bde5bc55e8fdfaaa62a2fa1d568a634
odan <odan3240@gmail.com>
2022-06-19 16:32:28 +0900

    デプロイコマンドを追加

---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#/!bin/bash
+
+set -eux
+
+git pull origin `git symbolic-ref --short HEAD`
+
+# --- app ---
+pushd nodejs
+
+npm ci
+npm run build
+
+popd
+
+sudo systemctl restart isucondition.nodejs.service
+
+# --- nginx ---
+
+sudo rm -f /var/log/nginx/access.log
+sudo rm -f /var/log/nginx/error.log
+
+sudo cp config/nginx-primary/nginx.conf /etc/nginx/nginx.conf
+sudo cp config/nginx-primary/isucondition.conf /etc/nginx/sites-available/isucondition.conf
+
+sudo systemctl restart nginx.service
+
+# --- MariaDB ---
+
+sudo cp config/mysql/my.cnf /etc/mysql/my.cnf
+sudo cp -r config/mysql/conf.d/ /etc/mysql/conf.d/
+sudo cp -r config/mysql/mariadb.conf.d/ /etc/mysql/mariadb.conf.d/
+
+sudo systemctl restart mysql.service
