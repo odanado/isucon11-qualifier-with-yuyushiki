a92b15d77ac019db60b7c6575048591690c20ff1
odan <odan3240@gmail.com>
2022-06-19 20:13:52 +0900

    ログを有効化

--- nodejs/src/setup-tracing.ts
+++ nodejs/src/setup-tracing.ts
@@ -2,5 +2,5 @@ import * as TraceAgent from "@google-cloud/trace-agent";
 
 export function setupTracing(name: string): TraceAgent.PluginTypes.Tracer {
   console.log({ name });
-  return TraceAgent.start({ enhancedDatabaseReporting: true });
+  return TraceAgent.start({ enhancedDatabaseReporting: true, logLevel: 4 });
 }
