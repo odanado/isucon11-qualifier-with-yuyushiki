96d88e826b9940f6b49af3274fd80acfd69f0d3e
mizdra <pp.mizdra@gmail.com>
2022-06-19 18:27:09 +0900

    デプロイスクリプト修正

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,5 +1,8 @@
 #/!bin/bash
 
-../bench/bench -all-addresses 127.0.0.11 -target 127.0.0.11:443 -tls -jia-service-url http://127.0.0.1:4999
-cp /var/log/nginx/access.log logs/access.log
-cp /var/log/mysql/mariadb-slow.log logs/mariadb-slow.log
+cd ../bench
+/bench -all-addresses 127.0.0.11 -target 127.0.0.11:443 -tls -jia-service-url http://127.0.0.1:4999
+
+cd ../webapp
+sudo cp /var/log/nginx/access.log logs/access.log
+sudo cp /var/log/mysql/mariadb-slow.log logs/mariadb-slow.log
