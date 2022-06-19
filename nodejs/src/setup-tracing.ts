f292139a307cfde94ba8ec81f18aa6f875b72f09
odan <odan3240@gmail.com>
2022-06-19 17:27:55 +0900

    @google-cloud/trace-agent でええやん

--- nodejs/src/setup-tracing.ts
+++ nodejs/src/setup-tracing.ts
@@ -1,28 +1,6 @@
-import opentelemetry, { Tracer } from "@opentelemetry/api";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
-import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import { TraceExporter } from "@google-cloud/opentelemetry-cloud-trace-exporter";
+import TraceAgent from "@google-cloud/trace-agent";
 
-export function setupTracing(name: string): Tracer {
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: name,
-    }),
-  });
-
-  registerInstrumentations({
-    tracerProvider: provider,
-    instrumentations: [getNodeAutoInstrumentations()],
-  });
-
-  const exporter = new TraceExporter();
-  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-
-  provider.register();
-
-  return opentelemetry.trace.getTracer(name);
+export function setupTracing(name: string): void {
+  console.log({ name });
+  TraceAgent.start();
 }
