3bfe874111a23fb9a91473c05951952574a41709
odan <odan3240@gmail.com>
2022-06-19 17:28:54 +0900

    実行時エラーになる

--- nodejs/src/setup-tracing.ts
+++ nodejs/src/setup-tracing.ts
@@ -1,4 +1,4 @@
-import TraceAgent from "@google-cloud/trace-agent";
+import * as TraceAgent from "@google-cloud/trace-agent";
 
 export function setupTracing(name: string): void {
   console.log({ name });
