886ed51aee7199c0f1c77c2aa67a54d56c569970
odan <odan3240@gmail.com>
2022-07-09 19:43:49 +0900

    さよなら @google-cloud/trace-agent

--- nodejs/src/setup-tracing.ts
+++ nodejs/src/setup-tracing.ts
@@ -1,11 +1,5 @@
-import * as TraceAgent from "@google-cloud/trace-agent";
 import tracer, { Tracer } from "dd-trace";
 import { execSync } from "child_process"
-
-export function setupTracing(name: string): TraceAgent.PluginTypes.Tracer {
-  console.log({ name });
-  return TraceAgent.start({ enhancedDatabaseReporting: true, samplingRate: 0 });
-}
 
 export function setupDatadog(): Tracer {
   const currentCommitHash = execSync("cd ~/webapp/nodejs/; git rev-parse HEAD").toString().trim()
