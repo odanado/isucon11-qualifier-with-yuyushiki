ea67e06ca0e1873f51add070d1162dba58c8dee8
mizdra <pp.mizdra@gmail.com>
2022-07-09 19:46:15 +0900

    conection を2倍にしてみる

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -136,7 +136,7 @@ const dbinfo: mysql.PoolOptions = {
   user: process.env["MYSQL_USER"] ?? "isucon",
   database: process.env["MYSQL_DBNAME"] ?? "isucondition",
   password: process.env["MYSQL_PASS"] || "isucon",
-  connectionLimit: 10,
+  connectionLimit: 20,
   timezone: "+09:00",
 };
 const pool = mysql.createPool(dbinfo);
